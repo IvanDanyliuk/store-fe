c2c786766879b66bc79e1130fa0cfa09f8f22520
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-09 20:09:39 +0200

    The Button component has been covered with tests

---
+++ src/app/components/ui/__tests__/Button.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { ButtonColor, ButtonType } from '../../../../types/types';
+import Button from '../Button';
+
+
+const clickHandler = jest.fn();
+
+describe('Tests for the Button component', () => {
+  test('should render the Button component', () => {
+    render(
+      <Button 
+        type={ButtonType.Button} 
+        color={ButtonColor.Primary} 
+        onClick={clickHandler}
+      >
+        Submit
+      </Button>
+    );
+    expect(screen.getByText(/Submit/)).toBeInTheDocument();
+  });
+});
