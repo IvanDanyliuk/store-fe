4f8f383d42a2d3d6d215e1b2443b961293791c2b
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-10 16:09:41 +0200

    The FormErrorMessage component has been covered with tests

---
+++ src/app/components/ui/__tests__/FormErrorMessage.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import FormErrorMessage from '../FormErrorMessage';
+
+
+describe('Tests for the FormErrorMessage component', () => {
+  test('should render the FormErrorMessage component', () => {
+    render(<FormErrorMessage error='Error' />);
+    expect(screen.getByText(/Error/)).toBeInTheDocument();
+  });
+});
