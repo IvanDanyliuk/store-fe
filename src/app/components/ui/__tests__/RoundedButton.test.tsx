b98f3858faf43b00efb11d7328d4d2a1a036aeed
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-10 17:06:38 +0200

    The RoundedButton component has been covered with tests

---
+++ src/app/components/ui/__tests__/RoundedButton.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { ButtonColor, ButtonType } from '../../../../types/types';
+import RoundedButton from '../RoundedButton';
+
+
+const clickHandler = jest.fn();
+
+describe('Tests for the RoundedButton component', () => {
+  test('should render the RoundedButton component', () => {
+    render(
+      <RoundedButton
+        type={ButtonType.Button}
+        color={ButtonColor.Primary}
+        onClick={clickHandler}
+      >
+        i
+      </RoundedButton>
+    );
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+});
