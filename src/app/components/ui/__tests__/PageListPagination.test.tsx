159d1b5183855cbbc419c88fa9dfd0be6a88d85f
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-10 16:52:32 +0200

    The PageListPagination component has been covered with tests

---
+++ src/app/components/ui/__tests__/PageListPagination.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import PageListPagination from '../PageListPagination';
+
+
+const clickHandler = jest.fn();
+
+describe('Tests for the PageListPagination component', () => {
+  beforeEach(() => {
+    render(
+      <PageListPagination 
+        currentPage={1} 
+        pageCount={10} 
+        setPage={clickHandler} 
+      />
+    );
+  });
+
+  test('the pageCount value should be equal to the number of navigation buttons', () => {
+    expect(screen.getAllByRole('button')).toHaveLength(10);
+  });
+
+  test('should call the clickHandler function when clicking on the page number button', () => {
+    fireEvent.click(screen.getByRole('button', { name: /2/ }));
+    expect(clickHandler).toHaveBeenCalled();
+  });
+});
