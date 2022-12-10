01b6f8860bfee46b8fa7090866f0a3d2334f408f
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-10 17:00:16 +0200

    The ProductListImage component has been covered with tests

---
+++ src/app/components/ui/__tests__/ProductListImage.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import ProductListImage from '../ProductListImage';
+
+
+describe('Tests for the ProductListImage component', () => {
+  test('should render the ProductListImage component', () => {
+    render(
+      <ProductListImage 
+        url='https://www.storage.com/images/image_1.png' 
+        altText='image_1' 
+      />
+    );
+    expect(screen.getByRole('img')).toBeInTheDocument();
+  });
+});
