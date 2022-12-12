42ead76c587dc7b4dbf486dcc065e004857c7d2c
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-12 15:47:21 +0200

    The Checkbox component has been covered with tests

---
+++ src/app/components/inputs/__tests__/Checkbox.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import Checkbox from '../Checkbox';
+
+
+const clickHandler = jest.fn();
+
+describe('Tests for the Checkbox component', () => {
+  test('should render the Checkbox component', () => {
+    render(
+      <Checkbox 
+        name='testName'
+        label='Test Label' 
+        checked
+        onChange={clickHandler}
+      />
+    );
+    expect(screen.getByText(/Test Label/)).toBeInTheDocument();
+  });
+});
