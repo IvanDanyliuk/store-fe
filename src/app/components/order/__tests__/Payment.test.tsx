4d40d3757bffe92aea89f26205f3cf8bc27ba403
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-18 18:18:22 +0200

    The Payment component has been covered with tests

---
+++ src/app/components/order/__tests__/Payment.test.tsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Payment from '../Payment';
+
+
+describe('The Payment component tests', () => {
+  test('should render the card form', () => {
+    render(<Payment />);
+    const cardFields = screen.getAllByRole('textbox');
+    userEvent.type(cardFields[0], '7777777777777777');
+    userEvent.type(cardFields[1], '01');
+    userEvent.type(cardFields[2], '12');
+    userEvent.type(cardFields[3], '777');
+    fireEvent.click(screen.getByRole('button', { name: 'paymentPayBtn' }));
+    expect(screen.getByRole('dialog')).toBeInTheDocument();
+  });
+});
