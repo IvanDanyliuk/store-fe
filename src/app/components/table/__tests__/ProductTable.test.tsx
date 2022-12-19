7d578b551e83fa2fbe6a26291f53c734ab1363f9
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-19 20:53:46 +0200

    The ProductTable component has been covered with tests

---
+++ src/app/components/table/__tests__/ProductTable.test.tsx
@@ -0,0 +1,97 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { IProduct } from '../../../features/product/types';
+import ProductTable from '../ProductTable';
+
+
+const productListMock: IProduct[] = [{
+  _id: 'product_id_1',
+  brand: 'Test Brand',
+  category: {
+    main: {
+      title: 'Main Category Name',
+      url: 'main-category-title'
+    },
+    subCategory: {
+      title: 'Sub-category name',
+      url: 'sub-category-name'
+    }
+  },
+  color: '#ffffff',
+  description: 'Test Descrinption',
+  image: 'https://www.storage.com/categories/test_product.png',
+  isInStock: true,
+  price: 1000,
+  promotion: ['TOP'],
+  rating: 5,
+  shortInfo: 'Test short information',
+  title: 'Test Title'
+}];
+
+const emptyProductListMock: IProduct[] = [];
+
+const editHandler = jest.fn();
+const deleteHandler = jest.fn();
+
+describe('Tests for the ProductTable component', () => {
+  test('should render a loader while the status value is loading', () => {
+    render(
+      <ProductTable 
+        products={productListMock} 
+        status='loading' 
+        onEdit={editHandler} 
+        onDelete={deleteHandler} 
+      />
+    );
+    expect(screen.getByTestId('loader')).toBeInTheDocument();
+  });
+
+  test('should render the empty product list message while the product list is empty', () => {
+    render(
+      <ProductTable 
+        products={emptyProductListMock} 
+        status='succeeded' 
+        onEdit={editHandler} 
+        onDelete={deleteHandler} 
+      />
+    );
+    expect(screen.getByText('productsNoDataMessage')).toBeInTheDocument();
+  });
+
+  test('should render the product list message while the status value is succeeded', () => {
+    render(
+      <ProductTable 
+        products={productListMock} 
+        status='succeeded' 
+        onEdit={editHandler} 
+        onDelete={deleteHandler} 
+      />
+    );
+    expect(screen.getByText(productListMock[0].title)).toBeInTheDocument();
+  });
+
+  test('should call the editHandler function after clicking on the Edit button', () => {
+    render(
+      <ProductTable 
+        products={productListMock} 
+        status='succeeded' 
+        onEdit={editHandler} 
+        onDelete={deleteHandler} 
+      />
+    );
+    fireEvent.click(screen.getByRole('button', { name: 'editBtn' }));
+    expect(editHandler).toHaveBeenCalled();
+  });
+
+  test('should call the editHandler function after clicking on the Edit button', () => {
+    render(
+      <ProductTable 
+        products={productListMock} 
+        status='succeeded' 
+        onEdit={editHandler} 
+        onDelete={deleteHandler} 
+      />
+    );
+    fireEvent.click(screen.getByRole('button', { name: 'deleteBtn' }));
+    expect(deleteHandler).toHaveBeenCalled();
+  });
+});
