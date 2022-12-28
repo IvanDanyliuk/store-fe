66c7a871a9da9becf9b4034b2f099d1e56a473e4
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-28 17:39:09 +0200

    The LegalInfo page has been covered with tests

---
+++ src/app/pages/__tests__/LegalInfo.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import LegalInfo from '../LegalInfo';
+
+
+describe('Tests for the LegalInfo page', () => {
+  test('shouldrender the LegalInfo page', () => {
+    render(<LegalInfo />);
+    expect(screen.getByRole('heading', { name: 'pageTitle' })).toBeInTheDocument();
+  });
+});
