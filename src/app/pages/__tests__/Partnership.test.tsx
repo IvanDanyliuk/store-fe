b4aaa0af0973834fd357353756a57e5347fe0b68
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-28 22:11:15 +0200

    The Partnership page has been covered with tests

---
+++ src/app/pages/__tests__/Partnership.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Partnership from '../Partnership';
+
+
+describe('Tests for the Partnership page', () => {
+  test('should render the Partnership page', () => {
+    render(<Partnership />);
+    expect(screen.getByRole('heading', { name: 'pageTitle' })).toBeInTheDocument();
+  });
+});
