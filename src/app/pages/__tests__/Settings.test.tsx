62d04150c3d95843e76590e7d446ce5f63a0b456
IvanDanyliuk <ivandaniliuk@gmail.com>
2022-12-29 21:02:26 +0200

    The Settings page has been covered with tests

---
+++ src/app/pages/__tests__/Settings.test.tsx
@@ -0,0 +1,11 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../../utils/testUtils';
+import Settings from '../Settings';
+
+
+describe('Tests for the Settings page', () => {
+  test('should render the Settings page', async () => {
+    renderWithProviders(<Settings />);
+    expect(screen.getAllByRole('link')).toHaveLength(5);
+  });
+});
