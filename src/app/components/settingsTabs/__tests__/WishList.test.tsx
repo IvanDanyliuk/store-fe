ac64381e4741086c25a47189e7399a4e8cba1fde
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-01-03 13:05:57 +0200

    The WishList component has been covered with tests

---
+++ src/app/components/settingsTabs/__tests__/WishList.test.tsx
@@ -0,0 +1,16 @@
+import { screen } from '@testing-library/react';
+import { setupProductSuccessHandlers } from '../../../../utils/serverMocks/product';
+import { setupUserSuccessHandlers } from '../../../../utils/serverMocks/user';
+import { renderWithProviders } from '../../../../utils/testUtils';
+import { productStateSuccess } from '../../../../utils/testDataMocks'
+import WishList from '../WishList';
+
+
+describe('Tests for the WishList component', () => {
+  test('should render product data from the wish list', () => {
+    setupProductSuccessHandlers();
+    setupUserSuccessHandlers();
+    renderWithProviders(<WishList />);
+    expect(screen.getByText(productStateSuccess.products.data[0].title)).toBeInTheDocument();
+  });
+});
