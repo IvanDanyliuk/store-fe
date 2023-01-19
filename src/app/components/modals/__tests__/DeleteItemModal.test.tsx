13ca1afe305af7a12954156e1a4b038ce0a1aff6
IvanDanyliuk <ivandaniliuk@gmail.com>
2023-01-19 13:38:09 +0200

    The DeleteItemModal component has been covered with unit tests

---
+++ src/app/components/modals/__tests__/DeleteItemModal.test.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, screen, render } from '@testing-library/react';
+import DeleteItemModal from '../DeleteItemModal';
+
+
+const deleteHandlerMock = jest.fn();
+const messageMock = 'Do you want to delete this item?';
+
+
+describe('Tests for the DeleteItemModal component', () => {
+  beforeEach(() => {
+    render(
+      <DeleteItemModal 
+        deleteHandler={deleteHandlerMock} 
+        message={messageMock}
+      />
+    );
+  });
+
+  test('should call the deleteHandler function by clicking on the Yes button', () => {
+    fireEvent.click(screen.getByRole('button', { name: 'deleteItemBtn' }));
+    fireEvent.click(screen.getByRole('button', { name: 'deleteItemYes' }));
+    expect(deleteHandlerMock).toHaveBeenCalled();
+  });
+
+  test('should close the modal by clicking the No button', () => {
+    fireEvent.click(screen.getByRole('button', { name: 'deleteItemBtn' }));
+    fireEvent.click(screen.getByRole('button', { name: 'deleteItemNo' }));
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+  });
+});
